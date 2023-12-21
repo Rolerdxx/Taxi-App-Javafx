4f425b546b33bf9fb4abce413962a5e119c00f05
Hamza-Elbouzidi <elbouzidihamza5@gmail.com>
2023-12-21 20:54:12 +0100

    Signupv2

--- src/main/java/com/example/uberlike/SignupPage.java
+++ src/main/java/com/example/uberlike/SignupPage.java
@@ -1,7 +1,8 @@
 package com.example.uberlike;
 
-import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -13,16 +14,19 @@ public class SignupPage {
     private Stage stage;
 
     @FXML
-    private TextField getinputname;
+    private TextField inputName;
 
     @FXML
-    private TextField getinputemail;
+    private TextField inputEmail;
 
     @FXML
-    private TextField getinputpassword;
+    private TextField inputPassword;
 
     @FXML
-    private TextField getinputtype;
+    private RadioButton driverRadio;
+
+    @FXML
+    private RadioButton passengerRadio;
 
     public SignupPage(Database db, Stage stage) {
         this.db = db;
@@ -31,19 +35,46 @@ public class SignupPage {
 
     @FXML
     protected void signUp() throws SQLException {
-        String name = getinputname.getText();
-        String email = getinputemail.getText();
-        String password = getinputpassword.getText();
-        String type = getinputtype.getText();
+        String name = inputName.getText();
+        String email = inputEmail.getText();
+        String password = inputPassword.getText();
+        String type = (driverRadio.isSelected()) ? "Driver" : "Passenger";
+
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            showAlert("All fields are required.");
+            return;
+        }
+
+        if (!isValidEmail(email)) {
+            showAlert("Invalid email format.");
+            return;
+        }
+
+        if (password.length() < 8) {
+            showAlert("Password must be at least 8 characters long.");
+            return;
+        }
+
 
         db.createUser(name, email, password, type);
 
-
         closeWindow();
-        stage.close();
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     private void closeWindow() {
         stage.close();
+    }
+
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        return email.matches(emailRegex);
     }
 }
